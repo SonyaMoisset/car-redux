bc69b5cbba9090249d7c6ef122f604e278c28988
smoisset <smoisset@worldremit.com>
2017-09-16 18:39:58 +0100

    Set up Redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
 import { BrowserRouter, Route } from 'react-router-dom'
 import './app.css'
+
+import reducers from './reducers'
 
 import App from './components/app'
 import Car from './components/car'
 
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
 ReactDOM.render(
-    <BrowserRouter>
-        <div>
-            <Route exact path="/" component={App}></Route>  
-            <Route exact path="/car/:id" component={Car}></Route>  
-        </div>    
-    </BrowserRouter>,
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Route exact path="/" component={App}></Route>
+                <Route exact path="/car/:id" component={Car}></Route>
+            </div>
+        </BrowserRouter>    
+    </Provider>,
     document.getElementById('root')
 )
