69d99afc144a90129d758713f48ad7414cb33451
smoisset <smoisset@worldremit.com>
2017-09-16 18:29:40 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+import './app.css'
+
+import App from './components/app'
+import Car from './components/car'
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Route exact path="/" component={App}></Route>  
+            <Route exact path="/car/:id" component={Car}></Route>  
+        </div>    
+    </BrowserRouter>,
+    document.getElementById('root')
+)
